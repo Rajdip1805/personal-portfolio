a86f12cee3383bae8cad98eb4f5838fe8a3cb82a
Rajdip Rajput <rjrajput5462@gmail.com>
2025-02-27 18:58:12 +0530

    initial commit

---
+++ src/portfolio.js
@@ -0,0 +1,66 @@
+const header = {
+  homepage: 'https://rjshkhr.github.io/cleanfolio',
+  title: 'RR',
+}
+
+const about = {
+  name: 'Rajdip Rajput',
+  role: 'IT Engineer',
+  description:
+    'Seeking a position in the field of IT where I can utilize my skills to further work towards personal and professional development and contribute towards the prosperity of the organization. A strong communicator and collaborator who is able to work effectively in both team and individual settings also confident with skills necessary to be a valuable to the team.',
+  resume:
+    'https://drive.google.com/file/d/12aQHMCoTeb31VQXuDi5c7q5QyUQba114/view?usp=sharing',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/rajdiprajput18/',
+    github: 'https://github.com/Rajdip1805',
+  },
+}
+
+const projects = [
+  {
+    name: 'Stock Market Predction',
+    description:
+      'LSTM-based model to predict stock prices with accuracy of above 70%',
+    stack: ['Python', 'Pandas', 'ML'],
+    sourceCode: 'https://github.com/Rajdip1805/Stock-market-Prediction',
+    livePreview: 'https://github.com/Rajdip1805/Stock-market-Prediction',
+  },
+  {
+    name: 'Project 2',
+    description:
+      'CryptoHub, a real-time cryptocurrency tracking platform providing live price updates, market trends, and historical data for 9,000+ cryptocurrencies.',
+    stack: ['React', 'Tailwind CSS', 'API'],
+    sourceCode: 'https://github.com/Rajdip1805/CryptoHub',
+    livePreview: 'https://cryptohubexplorer.netlify.app/',
+  },
+  {
+    name: 'AI Chatbot',
+    description:
+      'An intelligent chatbot to facilitate conversational interactions with context awareness and response accuracy',
+    stack: ['Python', 'TensorFlow', 'Json'],
+    sourceCode: 'https://github.com/Rajdip1805/AI-chatbot',
+    livePreview: 'https://github.com/Rajdip1805/AI-chatbot',
+  },
+]
+
+const skills = [
+  'C++',
+  'Python',
+  'DSA',
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Java',
+  'MySQL',
+  'OOPS',
+  'Git',
+  'OS',
+  'DBMS',
+]
+
+const contact = {
+  email: 'rjrajput5462@mail.com',
+}
+
+export { header, about, projects, skills, contact }
